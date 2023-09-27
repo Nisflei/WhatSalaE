6f20e0da486d764cda21aa019118125d1e48a8a4
Nisflei <ncgaloni@hotmail.com>
2023-09-26 21:01:13 -0300

    Toolbar+ABAS

--- app/src/main/java/com/aulas/whatsalae/MainActivity.java
+++ app/src/main/java/com/aulas/whatsalae/MainActivity.java
@@ -3,6 +3,7 @@ package com.aulas.whatsalae;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,11 +11,18 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.aulas.whatsalae.config.ConfigFirebase;
+import com.aulas.whatsalae.fragments.ContatosFragment;
+import com.aulas.whatsalae.fragments.ConversasFragment;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth autenticacao;
+    private FirebaseAuth autenticacao;
+
+    private FragmentPagerItemAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +32,20 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("WhatSalaE");
         // versoes antigas do android
         setSupportActionBar(toolbar);
+
+        // Criando as ABAS
+
+        adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Conversas", ConversasFragment.class)
+                .add("Contatos", ContatosFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
 
     }
 
